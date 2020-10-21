3a495176d03d8b47d73545de3e7eb1d38b0d8041
Felix <61483651+Elixoo@users.noreply.github.com>
2020-10-21 18:07:11 -0400

    Update letspam.py

--- letspam.py
+++ letspam.py
@@ -1,8 +1,40 @@
-import time
 import pyautogui
+import time
+import sys
 
-time.sleep(5)
-f = open("bees", 'r')
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
+a = input("Choose Spam: "
+          "[1]Bee_Movie "
+          "[2]Baby-Justin_Be-beer ")
+
+print("10")
+time.sleep(1)
+print("9")
+time.sleep(1)
+print("8")
+time.sleep(1)
+print("7")
+time.sleep(1)
+print("6")
+time.sleep(1)
+print("5")
+time.sleep(1)
+print("4")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+
+if a == "1":
+    wrd = open("bees", 'r')
+    for word in wrd:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+else:
+    if a == "2":
+        wrd = open("baby", 'r')
+        for word in wrd:
+            pyautogui.typewrite(word)
+            pyautogui.press("enter")
